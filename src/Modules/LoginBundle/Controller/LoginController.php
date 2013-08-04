e121fe36dceea97f5042fcf92813d24d57fa94e9
joao <joao@joao-GeForce6100PM-M2.(none)>
2013-08-04 19:06:02 -0300

    primero commit

---
+++ src/Modules/LoginBundle/Controller/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\LoginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/login")
+ * @Template()
+ */
+class LoginController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction($name)
+    {
+        return array('name' => $name);
+    }
+}

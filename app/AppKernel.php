d078682bc4c24eb14da2025137fc77806744f7ce
joaoluz <joaobatistadaluz@mail.com>
2013-08-29 01:11:29 -0300

    Atualizando registros de bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Modules\LoginBundle\ModulesLoginBundle(),
             new Modules\LayoutBundle\ModulesLayoutBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {

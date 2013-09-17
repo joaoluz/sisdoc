aca162861beb92a9651a0714bba5ea4b702411a3
joaoluz <joaobatistadaluz@mail.com>
2013-09-16 23:20:40 -0300

    Atualizando assets da aplicação

--- src/Modules/LoginBundle/Resources/js/sisloja/produto/index.js
+++
@@ -1,8 +0,0 @@
-var index = {
-   editar:function(id){
-       window.location = "/produto/produto/edit/id/"+id;
-   }
-}
-$(document).ready(function(){
-    grid.carrega($('#tabela-pesquisa-produto'), $('#form-produto'));
-});

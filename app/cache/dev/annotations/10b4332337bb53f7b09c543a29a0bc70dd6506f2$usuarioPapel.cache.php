b57da7239a95edc795b8d38e3a0de36dc47b3642
joaoluz <joao.luz@xys.com.br>
2014-01-22 12:32:28 -0200

    Removendo caches

--- app/cache/dev/annotations/10b4332337bb53f7b09c543a29a0bc70dd6506f2$usuarioPapel.cache.php
+++
@@ -1,1 +0,0 @@
-<?php return unserialize('a:1:{i:0;O:30:"Doctrine\\ORM\\Mapping\\OneToMany":6:{s:8:"mappedBy";s:9:"idUsuario";s:12:"targetEntity";s:46:"Modules\\AuthenticateBundle\\Entity\\UsuarioPapel";s:7:"cascade";N;s:5:"fetch";s:4:"LAZY";s:13:"orphanRemoval";b:0;s:7:"indexBy";N;}}');
